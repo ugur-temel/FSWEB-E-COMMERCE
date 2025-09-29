3771a5480b9aee256fdfe7c353296c2562e8d92a
ugur-temel <temelu091@gmail.com>
2025-09-29 23:14:53 +0300

    project is started

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <p>
+        HELLO! FSWEB-E-COMMERCE PROJECT!
+      </p>
+    </>
+  )
+}
+
+export default App
